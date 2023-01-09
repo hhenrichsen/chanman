3be35acbd629ecfdebb2fdce64c965bba447c478
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-01-09 11:49:39 -0700

    :recycle: Cache guild settings from the DB

--- bot/src/repositories/GuildSettings.repository.ts
+++ bot/src/repositories/GuildSettings.repository.ts
@@ -5,6 +5,10 @@ import { RepositoryManager } from "./Repository";
 
 @Service()
 export class GuildSettingsRepository extends RepositoryManager<GuildSettings> {
+    private readonly cache = new Map<string, GuildSettings>();
+    private readonly live = new Map<string, Date>();
+    private readonly timeoutMs = 5 * 60 * 1000;
+
     constructor(dataSourceManager: DataSourceManager) {
         super(GuildSettings, dataSourceManager);
     }
@@ -20,15 +24,30 @@ export class GuildSettingsRepository extends RepositoryManager<GuildSettings> {
             id,
         });
         await this.saveGuildSettings(settings);
+        this.cache.set(id, settings);
     }
 
     public async getGuildById(id: string): Promise<GuildSettings | undefined> {
+        const lastAccessed = this.live.get(id);
+        if (lastAccessed) {
+            if (
+                (new Date().getTime() - lastAccessed.getTime()) / 1000 <=
+                this.timeoutMs
+            ) {
+                return this.cache.get(id);
+            }
+        }
         return this.withRepo(async (repo) => {
-            return (await repo.findOne({ where: { id } })) ?? undefined;
+            const value = (await repo.findOne({ where: { id } })) ?? undefined;
+            value && this.cache.set(id, value);
+            value && this.live.set(id, new Date());
+            return value;
         });
     }
 
     public async saveGuildSettings(guild: GuildSettings) {
+        this.live.set(guild.id, new Date());
+        this.cache.set(guild.id, guild);
         return this.withRepo(async (repo) => {
             return await repo.save(guild);
         });
